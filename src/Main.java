bdc583abb3ba78544065a861f328f7d7a040d43d
Gabriel <usr4035@salesianos-lacuesta.net>
2025-11-17 12:56:17 +0000

    main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 package net.salesianos.sincronized;
 
-import net.salesianos.sincronized.threads.Productor;
-import net.salesianos.sincronized.threads.Consumidor;
+
+import net.salesianos.Threads.Consumidor;
+import net.salesianos.sincronized.Product;
+import net.salesianos.sincronized.StoreEmpanada;
 
 public class Main {
     public static void main(String[] args) {
 
         StoreEmpanada store = new StoreEmpanada(10);
 
-        Productor productor = new Productor(store, 20);
+        Product productor = new Productor(store, 20);
         Consumidor consumidor = new Consumidor(store, 20);
 
         Thread hiloProductor = new Thread(productor, "Productor");
