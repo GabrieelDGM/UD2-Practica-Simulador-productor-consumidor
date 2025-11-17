b779f790966204cc4d3b138c2dc2cb9ebb411b5d
Gabriel <usr4035@salesianos-lacuesta.net>
2025-11-17 13:34:40 +0000

    feat:Se agrego el experimiento tres y funciona correctamente

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,14 @@ import net.salesianos.sincronized.StoreEmpanada;
 public class Main {
     public static void main(String[] args) {
 
-        StoreEmpanada store = new StoreEmpanada(10);
+        StoreEmpanada store = new StoreEmpanada(5);
 
-        Productor productor = new Productor(store, 20);
-        Consumidor consumidor = new Consumidor(store, 20);
+        Productor productor1 = new Productor(store, 20);
+        Productor productor2 = new Productor(store, 20);
+        Consumidor consumidor = new Consumidor(store, 40);
 
-        Thread hiloProductor = new Thread(productor, "Productor");
-        Thread hiloConsumidor = new Thread(consumidor, "Consumidor");
-
-        hiloProductor.start();
-        hiloConsumidor.start();
+        new Thread(productor1, "Productor 1").start();
+        new Thread(productor2, "Productor 2").start();
+        new Thread(consumidor, "Consumidor").start();
     }
 }
