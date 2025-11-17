0ce8059a95957b2fecb85a343bc07a017c41a811
Gabriel <usr4035@salesianos-lacuesta.net>
2025-11-17 13:04:31 +0000

    feat:Se agrego la clase productor con sus propidades correspondiente

--- src/net/salesianos/Threads/Consumidor.java
+++ src/net/salesianos/Threads/Consumidor.java
@@ -1,29 +1,28 @@
-package net.salesianos.Threads;
-
-import net.salesianos.sincronized.Product;
 import net.salesianos.sincronized.StoreEmpanada;
 
-public class Consumidor implements Runnable {
-    private final StoreEmpanada store;
-    private final int cantidadConsumir;
+package net.salesianos.synchronized.threads;
 
-    public Consumidor(StoreEmpanada store, int cantidadConsumir) {
+import net.salesianos.synchronized.StoreEmpanada;
+
+public class Consumidor implements Runnable {
+
+    private final StoreEmpanada store;
+    private final int cantidad;
+
+    public Consumidor(StoreEmpanada store, int cantidad) {
         this.store = store;
-        this.cantidadConsumir = cantidadConsumir;
+        this.cantidad = cantidad;
     }
 
     @Override
     public void run() {
         try {
-            for(int i = 1; <= cantidadConsumir; i++)
-            product emp = store.consume();
-                System.out.println("El cliente consumio " + emp);
-                Thread.sleep(1000);
-
-
-        } catch (interruptedException e) {
+            for (int i = 0; i < cantidad; i++) {
+                store.consume();
+                Thread.sleep(350);
+            }
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-
-}
+    }
 }
---
+++ src/net/salesianos/Threads/Productor.java
@@ -0,0 +1,30 @@
+package net.salesianos.Threads;
+
+import net.salesianos.sincronized.Product;
+import net.salesianos.sincronized.StoreEmpanada;
+
+public class Productor implements Runnable {
+    private final StoreEmpanada store;
+    private final int cantidad;
+    private String[] types ={"Queso","Pollo","Pabellon","Carne","Jamon y queso"};
+    public Productor(StoreEmpanada store, int cantidad) {
+        this.store = store;
+        this.cantidad = cantidad;
+    }
+     @Override
+    public void run() {
+        try {
+            for (int i = 0; i < cantidad; i++) {
+                String tipo = tipos[(int)(Math.random() * tipos.length)];
+                Product empanada = new Product(tipo);
+                store.produce(empanada);
+                Thread.sleep(300);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+
+
