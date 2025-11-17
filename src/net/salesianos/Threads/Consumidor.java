f2da917358bc8a6c2ab69398dc38e2086b3165ab
Gabriel <usr4035@salesianos-lacuesta.net>
2025-11-17 11:57:46 +0000

    feat:Se creo la clase consumidor juntos con sus propiedades y metodos

--- src/net/salesianos/Threads/Consumidor.java
+++ src/net/salesianos/Threads/Consumidor.java
@@ -1,5 +1,29 @@
 package net.salesianos.Threads;
 
-public class Consumidor {
-    
+import net.salesianos.sincronized.Product;
+import net.salesianos.sincronized.StoreEmpanada;
+
+public class Consumidor implements Runnable {
+    private final StoreEmpanada store;
+    private final int cantidadConsumir;
+
+    public Consumidor(StoreEmpanada store, int cantidadConsumir) {
+        this.store = store;
+        this.cantidadConsumir = cantidadConsumir;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for(int i = 1; <= cantidadConsumir; i++)
+            product emp = store.consume();
+                System.out.println("El cliente consumio " + emp);
+                Thread.sleep(1000);
+
+
+        } catch (interruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+}
 }
--- src/net/salesianos/Threads/Product.java
+++ src/net/salesianos/Threads/Product.java
@@ -1,29 +1,31 @@
 package net.salesianos.Threads;
 
+import net.salesianos.sincronized.Product;
 import net.salesianos.sincronized.StoreEmpanada;
 
 public class Product implements Runnable {
     private final StoreEmpanada store;
     private final int cantidadProducir;
 
-    private final String[] types = {"Queso","Pollo","Pabellon","Carne","Jamon y queso"};
+    private final String[] types = { "Queso", "Pollo", "Pabellon", "Carne", "Jamon y queso" };
 
     public Product(StoreEmpanada store, int cantidadProducir) {
         this.store = store;
         this.cantidadProducir = cantidadProducir;
     }
+
+    @Override
     public void run() {
         try {
-            for( int i = 1; i <= cantidadProducir; i++) {
+            for (int i = 1; i <= cantidadProducir; i++) {
                 String type = types[i % types.length];
-                Product product = new Product(store, cantidadProducir);
-                store.produce(product);
+                store.produce(new Product(store, 1));
 
                 Thread.sleep(500);
-            
+
             }
-            
-        } catch (Exception e) {
+
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
